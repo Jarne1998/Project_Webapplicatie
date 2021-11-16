ca8ffe57c41b12d7d5e2ba136daff7fbba658232
Jarne1998 <r0781813@student.thomasmore.be>
2021-11-16 22:39:11 +0100

    toevoegen van path in navbar van Playlist

---
+++ MusicPlayer_Project/MusicPlayer_Project/Controllers/PlaylistController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicPlayer_Project.Data;
+using MusicPlayer_Project.Models;
+using MusicPlayer_Project.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicPlayer_Project.Controllers
+{
+    public class PlaylistController : Controller
+    {
+        private readonly MusicPlayer_ProjectContext _context;
+
+        public PlaylistController(MusicPlayer_ProjectContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            PlaylistListViewModel viewModel = new PlaylistListViewModel();
+            //viewModel.Playlists = _context.Playlists
+            return View(viewModel);
+        }
+    }
+}

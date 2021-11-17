a401ae08ccf898e044c05f4357e3a4053b9e82c2
Jarne1998 <r0781813@student.thomasmore.be>
2021-11-17 14:14:36 +0100

    Aanpasen van create voor playlists

--- MusicPlayer_Project/MusicPlayer_Project/Controllers/PlaylistController.cs
+++ MusicPlayer_Project/MusicPlayer_Project/Controllers/PlaylistController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using MusicPlayer_Project.Data;
 using MusicPlayer_Project.Models;
 using MusicPlayer_Project.ViewModels;
@@ -40,6 +41,89 @@ namespace MusicPlayer_Project.Controllers
                 vm.Playlists = _context.Playlists.ToList();
                 return View("Index", vm);
             }
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreatePlaylistViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(new Playlist()
+                {
+                    Name = viewModel.Name,
+                    DateAdded = viewModel.DateCreated,
+                });
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var playlist = await _context.Playlists.FindAsync(id);
+            if (playlist == null)
+            {
+                return NotFound();
+            }
+
+            EditPlaylistViewModel viewModel = new EditPlaylistViewModel()
+            {
+                PlaylistID = playlist.PlaylistID,
+                DateAdded = playlist.DateAdded,
+                Name = playlist.Name
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, EditPlaylistViewModel viewModel)
+        {
+            if (id != viewModel.PlaylistID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Playlist klant = new Playlist()
+                    {
+                        PlaylistID = viewModel.PlaylistID,
+                        Name = viewModel.Name,
+                        DateAdded = viewModel.DateAdded
+                    };
+                    _context.Update(klant);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Playlists.Any(e => e.PlaylistID == viewModel.PlaylistID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(viewModel);
         }
     }
 }
